299fc5b15eb57f7dbd8fafab5a168e300fea05ca
Amilcar Pimentel <amilcaroswaldo10@gamil.com>
2024-01-03 21:25:51 -0600

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,15 +4,16 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: "88887-11" }
   ])
+  let auxPerson;
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] = useState('')
   const addName = (event) => {
     event.preventDefault()
-    const findDuplicates= persons.findIndex(person => person.name ===newName)
-    if (findDuplicates !==-1) {
+    const findDuplicates = persons.findIndex(person => person.name === newName)
+    if (findDuplicates !== -1) {
       alert(`${newName} is already added to phonebook`)
     }
-    else{
+    else {
       const objName = {
         name: newName, number: newNumber
       }
@@ -27,22 +28,36 @@ const App = () => {
     const sourceNumber = event.target.value
     setNumber(sourceNumber)
   }
+  const handleFilterPersons = event => {
+    const name = event.target.value
+    const lower = String(name).toLowerCase();
+    const filterPerson = persons.filter(person => {
+      const nameList = person.name.toString().toLowerCase()
+      return nameList.includes(lower)
+    })
+    auxPerson = persons
+    console.log(name, auxPerson);
+    if (filterPerson.length > 0) setPersons(filterPerson)
+    if (name === "") setPersons(auxPerson)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with: <input onChange={handleFilterPersons} />
+      <br />
       <form onSubmit={addName}>
         <div>
-          name: <input  onChange={handleNameChange} value={newName}/>
-          number: <input  onChange={handleNumberChange} value={newNumber}/>
+          name: <input onChange={handleNameChange} value={newName} />
+          number: <input onChange={handleNumberChange} value={newNumber} />
         </div>
         <div>
           <button
-          type="submit">add</button>
+            type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person,i) => 
+        {persons.map((person, i) =>
           <li key={i}> {person.name} {person.number}</li>
         )}
       </ul>
